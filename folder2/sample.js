1dd0862f472ad010bc3980e23ee6a803f7e18204
durgaprasad9848 <pulipatiprasad2000@gmail.com>
2022-11-26 18:56:31 +0530

    task 2

---
+++ folder2/sample.js
@@ -0,0 +1,68 @@
+ 
+//2
+
+window.onload = function(){
+
+
+
+  var header= document.getElementById("header-title");
+
+  
+
+  header.textContent="Registration page";
+
+  console.log(header);
+
+ 
+
+}
+
+
+
+
+
+//3.
+
+
+
+//I)
+
+Innertext => Displays only inner text content of a tag.
+
+Textcontet => display whole the text inside the tag
+
+ 
+
+//II)
+
+
+
+var header= document.getElementById("header-title");
+
+header.style.border='2px solid black';
+
+
+
+
+
+//III)
+
+window.onload = function(){
+
+
+
+   const x = document.getElementById("items");
+
+   x.style.fontWeight="bold";
+
+   x.style.color="green";
+
+
+
+
+
+   console.log(x);
+
+    
+
+}
