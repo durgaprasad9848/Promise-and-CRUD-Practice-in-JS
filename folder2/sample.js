7db2ef7d60f3aede62a5e95608da9cdb4b7a9967
durgaprasad9848 <pulipatiprasad2000@gmail.com>
2022-12-03 14:55:58 +0530

    task 13

--- folder2/sample.js
+++ folder2/sample.js
@@ -1,55 +1,70 @@
-const submitButton = document.getElementById("submit");
- 
+var form = document.querySelector("#my-form");
+var nameInput = document.querySelector('#name');
+var emailInput = document.querySelector('#email');
+//console.log(form);
+//let userList = JSON.parse(localStorage.getItem('user'+user.email));
+//console.log(userList);
+form.addEventListener('submit', (e) =>{;
+    e.preventDefault();
+    // user object 
+    let user ={
+        name : nameInput.value,
+        email : emailInput.value,
+    } 
+    //console.log(user);
+    //userList.push(user);// array to store all the data of the users
+    document.querySelector("#my-form").reset();// to reset the form
+    //console.log(userList);
+    userSeralized = JSON.stringify(user);// local storage only allow string so convert into string.
+    localStorage.setItem( user.email ,userSeralized);// adding element in local storage
 
-if (document.readyState !== "loading") {
- 
-  var keys = Object.keys(localStorage), //taking out all the keys that are there in the local storage
-    i = keys.length; //6
-  //console.log("keys", keys);
-  let stringifiedDetailsOfPeople, detailsOfPeople;
-
-  // 6 to 0
-  Object.keys(localStorage).forEach((key) => {
-    //i==2
-    if (key.match(/userDetails/g)) {
-      //we only care about keys that start with userDetails
-      //this is called regex matching
-      stringifiedDetailsOfPeople = localStorage.getItem(key);
-      //console.log("stringifiedDetailsOfPeople", stringifiedDetailsOfPeople);
-      detailsOfPeople = JSON.parse(stringifiedDetailsOfPeople);
-     // console.log("details", detailsOfPeople);
-
-      addNewLineElement(detailsOfPeople);
-    }
-  });
-}
-// const listOfPeople = []
-submitButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  const emailId = document.getElementById("email").value;
-  const name = document.getElementById("name").value;
-  if (emailId.length > 0 && name.length > 0) {
-    const object = {
-      name: name,
-      emailId: emailId //unique
-    };
-    localStorage.setItem("userDetails" + emailId, JSON.stringify(object));
-    // localStorage.setItem("userDetailEmail" + emailId, emailId);
-    // listOfPeople.push(object)
-    addNewLineElement(object);
-  }
+    addListOfUsers(user);// calling the funtion
 });
 
-function addNewLineElement(object) {
-  const ul = document.getElementById("listOfPeople");
-  const li = document.createElement("li");
-  li.appendChild(
-    document.createTextNode(object.name + " " + object.emailId + " ")
-  );
-  // li.appendChild()
-  
+//userList_Diseralized = JSON.parse(localStorage.getItem('user'));
 
-   
+//console.log(userList_Diseralized)
+/*
+Array.from(userList_Diseralized).forEach(function(item){
+    //console.log(item);
+})
+*/
+function addListOfUsers(user) {
+    // get element where you wnat to create a elemnt 
+    var ul = document.getElementById("listOfUser");
+    // once get the element then create a element 
+    var li = document.createElement("li");
+    // append the elemet in child, child is because the we are creating the element so it will child of the the element we want ot add the element.
+    li.appendChild(document.createTextNode(user.name + " " + user.email + " "));
+    // append child node in paprent element or the where you want to add the element.
 
-  ul.appendChild(li);
+    //edit button
+    var editB = document.createElement('input');
+    editB.type = 'button'
+    editB.value = 'Edit'
+    editB.addEventListener('click',(e)=>{
+        document.getElementById('name').value = user.name;
+        document.getElementById('email').value = user.email;
+        li.remove();
+    })
+    editB.style.border = "2px solid green";
+    //delete button
+    var deleteB = document.createElement('input');
+    deleteB.type = 'button'
+    deleteB.value = 'Delete'
+    deleteB.addEventListener('click',(e) =>{
+        localStorage.removeItem(user.email);
+        li.remove();
+    })
+    deleteB.style.border = "2px solid red";
+    //
+    li.appendChild(editB);
+    li.appendChild(deleteB);
+    ul.append(li)
 }
+//userList.forEach(addListOfUsers);
+Object.keys(localStorage).forEach((key) => {
+    stringifiedDetailsOfPeople = localStorage.getItem(key);
+    detailsOfPeople = JSON.parse(stringifiedDetailsOfPeople);    
+    addListOfUsers(detailsOfPeople);
+});
